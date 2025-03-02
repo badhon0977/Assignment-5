29c95d80fc5a1496c6949f0dcb1b578304e99d58
badhon0977 <das007badhon@gmail.com>
2025-03-02 12:48:31 +0600

    pageShift js done

--- Javascript file/date.js
+++ Javascript file/date.js
@@ -1,38 +1,3 @@
-// function updateDate() {
-//   let today = new Date();
-
-//   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-//   let dayName = days[today.getDay()];
-
-//   let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-//   let monthName = months[today.getMonth()];
-
-//   let date = today.getDate();
-//   let year = today.getFullYear();
-
-//   document.getElementById("date").innerHTML =
-//       `<p class="text-[#00303C] text-[1.375rem] font-normal">${dayName}, <br>
-//           <span class="font-bold text-[1.375rem]">${monthName} ${date} ${year}</span>
-//       </p>`;
-// }
-
-// function scheduleMidnightUpdate() {
-//   let now = new Date();
-//   let midnight = new Date();
-//   midnight.setHours(24, 0, 0, 0);
-//   let timeUntilMidnight = midnight.getTime() - now.getTime();
-
-//   console.log(`Next update in: ${Math.round(timeUntilMidnight / 1000)} seconds`);
-
-//   setTimeout(() => {
-//       updateDate();
-//       scheduleMidnightUpdate();
-//   }, timeUntilMidnight);
-// }
-
-// updateDate();
-// scheduleMidnightUpdate();
-
 function updateDate() {
   let today = new Date();
 
---
+++ Javascript file/pageShift.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("discover-btn")
+    .addEventListener("click", function () {
+      window.location.href = "blogs.html";
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    document.getElementById("back-btn").addEventListener("click", function (){
+        window.location.href = "index.html";
+    });
+});
+
